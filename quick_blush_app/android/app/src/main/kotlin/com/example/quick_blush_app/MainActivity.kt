ee61113ebab15a716c781bc5495612cae5cb50a3
Bhuban <bhuban2024das@gmail.com>
2026-03-14 13:03:32 +0530

    for deploy

--- quick_blush_app/android/app/src/main/kotlin/com/example/quick_blush_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.quick_blush_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ src/migrations/1773393411679-init.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Init1773393411679 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
